475f9e461f1ba4d1ca05e48bb837f93143c7bb4e
experimental <exp.demindp2001@mail.ru>
2023-04-25 21:51:34 +0300

    Added function ResetZeroRows

--- module/CLI/src/main.cpp
+++ module/CLI/src/main.cpp
@@ -1,6 +1,7 @@
 #include "Menu/MatrixMenu/MatrixMenu.h"
 #include "Menu/Function/CreateMatrix.h"
 #include "Menu/Function/ResetRow.h"
+#include "Menu/Function/ReplaceZeroRows.h"
 #include "Menu/Function/OutputMatrix.h"
 
 using namespace MatrixManipulation::CLI;
@@ -9,6 +10,7 @@ int main() {
     MatrixMenu menu({
         {"Create matrix", CreateMatrix},
         {"Reset row",         ResetRow},
+        {"Replace zero rows", ReplaceZeroRows},
         {"Output matrix", OutputMatrix}
     });
     menu.run();
